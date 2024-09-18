2bfdb580402af4dff592de02a7ac0fb79324cd6d
Frederic Hallein <Frederic.Hallein@axxes.com>
2024-09-18 16:31:48 +0200

    add homePage pageobject

---
+++ POMs/HomePage.ts
@@ -0,0 +1,17 @@
+import { expect, Locator, Page } from "@playwright/test";
+
+export class HomePage {
+    private readonly page: Page;
+    private readonly elementsButton: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this.elementsButton = page.getByRole('heading', { name: 'Elements'});
+    }
+    async clickElementsButton() { await this.elementsButton.click(); }
+
+    async goTo() {
+        await this.page.goto('https://demoqa.com/');
+        await expect(this.page).toHaveTitle("DEMOQA");
+    }
+}
--- tests/ui/example.spec.ts
+++ tests/ui/example.spec.ts
@@ -1,7 +1,17 @@
 import { test, expect } from '@playwright/test';
+import { HomePage } from '../../POMs/HomePage';
+
+//let homePage: HomePage;
 
 test('has title', async ({ page }) => {
-  await page.goto('https://www.youtube.com/');
-  await expect(page).toHaveTitle("YouTube");
+  const homePage = new HomePage(page);
+  await homePage.goTo();
 });
 
+
+test('Fill in Text box and submit', async ({ page }) => {
+  const homePage = new HomePage(page);
+  await homePage.goTo();
+  await homePage.clickElementsButton();
+  // TODO: add assert
+});
