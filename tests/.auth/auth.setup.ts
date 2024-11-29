d0c41876f18a6599b141869b3bad2cab490b9753
frederic-hallein <frederic.hallein@outlook.com>
2024-11-29 16:07:48 +0100

    finished tests

--- tests/.auth/auth.setup.ts
+++ tests/.auth/auth.setup.ts
@@ -3,7 +3,7 @@ import { test as setup, expect } from "../../fixtures.ts";
 import path from "path";
 const authFile = path.join(__dirname, "./user-auth.json");
 
-setup("authenticate", async ({ page, homePage }) => {
+setup("Authenticate", async ({ page, homePage }) => {
     await homePage.navigateTo();
     await expect(page).toHaveTitle("DEMOQA"); 
     await page.context().storageState({ path: authFile });
--- tests/api/book-store-tests.spec.ts
+++ tests/api/book-store-tests.spec.ts
@@ -1,57 +1,58 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
+import { user } from "../../data/user-credentials.json";
 
+function randomNumber(min: number, max: number) { return Math.floor(Math.random() * (max - min) + min); }
 
 test.use({ storageState: "./tests/.auth/user-auth.json" });
 test.describe("Tests of the 'Book Store Application - Book Store API'", () => {
-  
-  test('GET books', async ({ request }) => {
-    const getBooksResponse = await request.get("/Bookstore/v1/Books");
-    await expect(getBooksResponse).toBeOK();
-    console.log(getBooksResponse.body());
-  });
-
-  test('POST a user', async ({ request }) => {
-    const postUserResponse = await request.post("/Account/v1/User", 
-      {data: {
-          "userName": "string3",
-          "password": "Password123#"
-        }
-      })
+  test("Post a user with authorization", async ({ request }) => {
+    // POST user
+    const userNumber = randomNumber(1, 10000);
+    const postUserResponse = await request.post("/Account/v1/User", {
+      data: {
+        userName: user.userName + userNumber,
+        password: user.password
+      }
+    });
+    const postUserResponseJSON = await postUserResponse.json(); 
     await expect(postUserResponse).toBeOK();
-    // TODO : assertion 
-  });
-    
 
-  test('POST a book', async ({ request }) => {
-    const postBookResponse = await request.post("/Bookstore/v1/Books", 
-      {data: {
-        "userId": "string",
-        "collectionOfIsbns": [
-          {
-            "isbn": "string"
-          }
-        ]}
-      })
-    await expect(postBookResponse).toBeOK();
-    expect(await postBookResponse.json()).toEqual(
-      expect.objectContaining(
-        {
-          "userId": "string",
-          "collectionOfIsbns": [
-            {
-              "isbn": "string"
-            }]
-        }
-      )
-    );
+    // Generate token for authorization
+    const postTokenResponse = await request.post("/Account/v1/GenerateToken", {
+      data: {
+        userName: user.userName + userNumber,
+        password: user.password
+      }
+    });
+    const postTokenResponseJSON = await postTokenResponse.json(); 
+    await expect(postTokenResponse).toBeOK();
+
+    // Check if authorized
+    const postAuthorizationResponse = await request.post("/Account/v1/Authorized", {
+      data: {
+        userName: user.userName + userNumber,
+        password: user.password
+      }
+    });
+    const postAuthorizationResponseJSON = await postAuthorizationResponse.json(); 
+    await expect(postAuthorizationResponse).toBeOK();
   });
 
-  test('DELETE a user', async ({ request }) => {
+  test("Get a all and a single book", async ({ request }) => {
+    // GET all books
+    const getBooksResponse = await request.get("/Bookstore/v1/Books");
+    const getBooksResponseJSON = await getBooksResponse.json();
+    await expect(getBooksResponse).toBeOK();
 
-    
+    // GET first book
+    const ISBN = await getBooksResponseJSON.books[0].isbn
+    const getBookResponse = await request.get(`/Bookstore/v1/Book?ISBN=${ISBN}`);
+    const getBookResponseJSON = await getBookResponse.json();
+    await expect(getBookResponse).toBeOK();
   });
 
 
 
 });
+ 
